4ef041ce513a9a21894ee27c832bd420f68385a3
Sahitya <contact.sahitya@gmail.com>
2020-02-22 12:18:19 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@ from setuptools import setup
 setup(
     # Needed to silence warnings (and to be a worthwhile package)
     name='InstaScraper',
-    url='https://github.com/supmayu/',
+    url='https://supmanyu.github.io',
     author='Sahitya Upmanyu ',
     author_email='contact.sahitya@gmail.com',
     # Needed to actually package something
     packages=['release'],
     # Needed for dependencies
-    install_requires=['beautifulsoup4','requests','soupsieve','BeautifulSoup'],
+    install_requires=['beautifulsoup4','requests','soupsieve','BeautifulSoup','cfscrape'],
     # *strongly* suggested for sharing
-    version='0.1',
+    version='0.1.1',
     # The license can be anything you like
     license='GNU GPL V3',
-    description='An example of a python package from pre-existing code',
+    description='A Instagram email scraper written in python using beautifulSoup.',
     # We will also need a readme eventually (there will be a warning)
     long_description=open('README.md').read(),
 )
